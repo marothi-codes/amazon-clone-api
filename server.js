4ba4294cc4626599464551462b98e037a5e825dd
Marothi Mahlake <marothi.codes@gmail.com>
2021-11-01 13:00:06 +0200

    Product details route added.

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found." });
+  }
+});
+
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
