2ef66334cf5d0845f28117161a47fc15169844dc
Marothi Mahlake <marothi.codes@gmail.com>
2021-11-04 01:35:41 +0200

    HTTP 404 on product id corrected to HTTP 400

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get("/api/products/:id", (req, res) => {
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({ message: "Product not found." });
+    res.status(500).send({ message: "Product not found." });
   }
 });
 
