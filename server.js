7ac61c89217e4e9b44efcd32d77644a81ce1180f
Marothi Mahlake <marothi.codes@gmail.com>
2021-11-09 19:02:39 +0200

    Product model and router added.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import data from "./data.js";
 import mongoose from "mongoose";
 import userRouter from "./routers/userRouter.js";
+import productRouter from "./routers/productRouter.js";
 
 dotenv.config();
 
@@ -19,20 +19,7 @@ mongoose.connect(
 app.disable("x-powered-by");
 
 app.use("/api/users", userRouter);
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(400).send({ message: "Product not found." });
-  }
-});
-
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
+app.use("/api/products", productRouter);
 
 app.get("/", (req, res) => {
   res.send("The server is ready.");
