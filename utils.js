2fbba29d2e1f83ab89e8121a996e53c103899aa4
Marothi Mahlake <marothi.codes@gmail.com>
2021-11-21 18:52:10 +0200

    Check added to validate users against their assigned roled.

--- utils.js
+++ utils.js
@@ -30,19 +30,10 @@ export const isAuthenticated = (req, res, next) => {
   } else res.status(401).send({ message: "No token found." });
 };
 
-/**
- * Converts a string into a URL and SEO friendly slug.
- * @param {String} text The input text to convert into a slug.
- * @returns {String} The slug.
- */
-export const slugify = (text) => {
-  return text
-    .toString() // Called just to prevent any casting error.
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "") // Remove accents.
-    .toLowerCase() // Ensure that the whole string is all lowercase letters.
-    .replace(/\s+/g, "-") // Replace all spaces with hyphens.
-    .replace(/[^\w\-]+/g, "") // Remove all non-letter characters.
-    .replace(/^-+/, "") // trim the beginning of the string.
-    .replace(/-+$/, ""); // trim the end of the string as well...
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) next();
+  else
+    res
+      .status(401)
+      .send({ message: "You're unauthorized to access this content." });
 };
