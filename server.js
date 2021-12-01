cea28ac19c76d3c844d5443ebc91ccf009e51328
Marothi Mahlake <marothi.codes@gmail.com>
2021-12-01 07:17:46 +0200

    Google Maps API integrated.

--- server.js
+++ server.js
@@ -33,6 +33,9 @@ app.use("/api/orders", orderRouter);
 app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
+app.get("/api/config/google", (req, res) => {
+  res.send(process.env.GOOGLE_API_KEY);
+});
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
