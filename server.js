b60d0ac4d846e40f674c1f5fc9afa2711a3fceaf
Marothi Mahlake <marothi.codes@gmail.com>
2021-12-03 15:56:28 +0200

    Socket.io server implemented.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import { Server } from "socket.io";
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
@@ -50,6 +52,82 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () =>
-  console.log("The app is running on: http//:localhost:" + PORT)
-); // Runs the express server on the "PORT" specified.
+const httpServer = http.Server(app);
+const socket_io = new Server(httpServer, { cors: { origin: "*" } });
+const users = [];
+
+socket_io.on("connection", (socket) => {
+  console.log("connection", socket.id);
+  socket.on("disconnect", () => {
+    const user = users.find((x) => x.socketId === socket.id);
+    if (user) {
+      user.online = false;
+      console.log("Offline", user.name);
+      const admin = users.find((x) => x.isAdmin && x.online);
+      if (admin) {
+        socket_io.to(admin.socketId).emit("updateUser", user);
+      }
+    }
+  });
+
+  socket.on("onLogin", (user) => {
+    const updatedUser = {
+      ...user,
+      online: true,
+      socketId: socket.id,
+      messages: [],
+    };
+
+    const existUser = users.find((x) => x._id === updatedUser._id);
+
+    if (existUser) {
+      existUser.socketId = socket.id;
+      existUser.online = true;
+    } else {
+      users.push(updatedUser);
+    }
+
+    console.log("Online", user.name);
+    const admin = users.find((x) => x.isAdmin && x.online);
+    if (admin) {
+      socket_io.to(admin.socketId).emit("updateUser", updatedUser);
+    }
+    if (updatedUser.isAdmin) {
+      socket_io.to(updatedUser.socketId).emit("listUsers", users);
+    }
+  });
+
+  socket.on("onUserSelected", (user) => {
+    const admin = users.find((x) => x.isAdmin && x.online);
+    if (admin) {
+      const existUser = users.find((x) => x._id === user._id);
+      socket_io.to(admin.socketId).emit("selectUser", existUser);
+    }
+  });
+
+  socket.on("onMessage", (message) => {
+    if (message.isAdmin) {
+      const user = users.find((x) => x._id === message._id && x.online);
+      if (user) {
+        socket_io.to(user.socketId).emit("message", message);
+        user.messages.push(message);
+      }
+    } else {
+      const admin = users.find((x) => x.isAdmin && x.online);
+      if (admin) {
+        socket_io.to(admin.socketId).emit("message", message);
+        const user = users.find((x) => x._id === message._id && x.online);
+        user.messages.push(message);
+      } else {
+        socket_io.to(socket.id).emit("message", {
+          name: "Admin",
+          body: "Sorry. I am not online right now",
+        });
+      }
+    }
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`App is running on: http://localhost:${PORT}`);
+});
